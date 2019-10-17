885a83a3b58c7c24b5c2cd94efc566590eb4b354
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2019-10-17 19:31:03 -0300

    adicionando pergunda de unidade e nomenclarura do DBF no padrão do ERP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const xlsx = require('node-xlsx')
 const dbf = require('dbf')
+const readline = require('readline')
 const fs = require('fs')
+
+const now = new Date
+const monName = new Array ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12')
 
 function item(codbar, qtdemb1, embala1 ) {
     this.codbar = codbar
@@ -8,7 +12,13 @@ function item(codbar, qtdemb1, embala1 ) {
     this.embala1 = embala1
 }
 
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const config = fs.readFileSync(__dirname + '/config.json', 'utf-8')
+
 
 const plan = xlsx.parse(JSON.parse(config).path.input + '/dados.xls')
 
@@ -22,16 +32,20 @@ const lista = plan[0].data.map( function(e, i) {
 
 let buf = dbf.structure(lista)
 
-fs.writeFileSync(JSON.parse(config).path.output + '/dados.dbf', toBuffer(buf.buffer))
+leitor.question("Para qual unidade é: ", (unidade) => {
+    fs.writeFileSync(JSON.parse(config).path.output + 
+        '/tp' +unidade + '01'+ now.getFullYear() + monName[now.getMonth() ] + 
+        now.getDate() + '00.dbf', toBuffer(buf.buffer))
+    leitor.close()
+})
+
+fs.rename(JSON.parse(config).path.input + '/dados.xls' , JSON.parse(config).path.input + '/dados_old.xls', (e) => { })
 
 function toBuffer(ab) {
-    let buffer = new Buffer(ab.byteLength)
+    let buffer = new Buffer.alloc(ab.byteLength)
     let view = new Uint8Array(ab)
     for (var i = 0; i < buffer.length; ++i) {
         buffer[i] = view[i]
     }
     return buffer
 }
-
-
-
